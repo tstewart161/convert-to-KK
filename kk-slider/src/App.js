20d138a771c33166cb1b52b7e9d798048838ee4f
tstewart161 <1.61tstewart@gmail.com>
2020-05-02 10:40:25 -0400

    Added web app

---
+++ kk-slider/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+
+  return (
+    <div className="App">
+      Hello
+    </div>
+  );
+}
+
+export default App;
